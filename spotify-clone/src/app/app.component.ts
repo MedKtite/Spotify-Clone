d88ceed1a3ab50b4ab4bee1faf2756e4c70ad88d
Med KTITE <ktite.m3@gmail.com>
2024-12-24 22:09:50 +0100

    ✨🔧 Add utility classes to library & navigation component

--- spotify-clone/src/app/app.component.ts
+++ spotify-clone/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import { NavigationComponent } from "./layout/navigation/navigation.component";
+import { LibraryComponent } from "./layout/library/library.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule, NavigationComponent, LibraryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'spotify-clone';
+  private faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit() {
+    this.initFontAwesomeIcons();
+  }
+
+  private initFontAwesomeIcons() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+
+  }
 }
---
+++ spotify-clone/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,10 @@
+import {faBook, faCirclePlay, faHome, faPlus, faSearch} from "@fortawesome/free-solid-svg-icons";
+import {IconDefinition} from "@fortawesome/fontawesome-svg-core";
+
+export const fontAwesomeIcons : IconDefinition[] = [
+  faCirclePlay,
+  faHome,
+  faSearch,
+  faBook,
+  faPlus,
+  ]
